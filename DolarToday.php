d2887607bf84ae37b8d2e6f577440c79454ce8ad
rdymac <rdymac@rdymac.local>
2014-02-10 02:29:26 +0100

    check if DolarToday data is loaded
    check if DolarToday data is loaded and notify by email if not

--- DolarToday.php
+++ DolarToday.php
@@ -17,6 +17,18 @@ $xve_usd = $jsonDolarToday['USD']['efectivo'];
 
 $xve_eur = $jsonDolarToday['EUR']['efectivo'];
 
+if (!isset($xve_usd) || $xve_usd == '') {
+
+	$btcven_json_2 = json_decode(file_get_contents('btcven.json'),true) or die("can't get rates");
+	
+	$xve_usd = $btcven_json_2['exchange_rates']['XVE_USD'];
+	
+	$xve_eur = $btcven_json_2['exchange_rates']['XVE_EUR'];
+	
+	// Notify by email that DolarToday prices are not working
+	include('notify_mail.php');
+}
+
 if (isset($_GET['json']) && $_GET['json'] == 'yes') {
 	
 	header('content-type: application/json; charset=utf-8');
---
+++ notify_mail.php
@@ -0,0 +1,9 @@
+<?php
+
+$msg_title = '';
+
+$msg_message = '';
+ 
+mail('', $msg_title, $msg_message);
+
+?>

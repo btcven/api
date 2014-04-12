9e62f21008dac917796762d26439cee08104f3f1
rdymac <rdymac@rdymac.local>
2014-04-12 04:58:52 +0200

    Add Litecoin prices and historical
    Add Litecoin prices and historical

--- historical/index.php
+++ historical/index.php
@@ -30,22 +30,41 @@ $connection = mysqli_connect($db_server, $db_user, $db_password, $db_database);
 			
 			list($date, $item1) = explode(" ", $row['date']);
 			
+			// Bitcoin
+			
 			if (isset($row['vefbitcoin']) && $row['vefbitcoin'] != "" && $row['vefbitcoin'] != 0){
 				
-				$historical_XVE[$date] = str_replace(',', '', $row['vefbitcoin']);
+				$historical_XVE_BTC[$date] = str_replace(',', '', $row['vefbitcoin']);
 			}
 			
 			if (isset($row['arsbitcoin']) && $row['arsbitcoin'] != "" && $row['arsbitcoin'] != 0){
 							
-				$historical_XAR[$date] = $usd = str_replace(',', '', $row['arsbitcoin']);
+				$historical_XAR_BTC[$date] = $usd = str_replace(',', '', $row['arsbitcoin']);
+			}
+			
+			// Litecoin
+			
+			if (isset($row['veflitecoin']) && $row['veflitecoin'] != "" && $row['veflitecoin'] != 0){
+							
+				$historical_XVE_LTC[$date] = $usd = str_replace(',', '', $row['veflitecoin']);
+			}
+			
+			if (isset($row['arslitecoin']) && $row['arslitecoin'] != "" && $row['arslitecoin'] != 0){
+							
+				$historical_XAR_LTC[$date] = $usd = str_replace(',', '', $row['arslitecoin']);
 			}
 		}	
 	}
 
 mysqli_close($connection);
 
-$historical['VEF_BTC'] = $historical_XVE;
-$historical['ARS_BTC'] = $historical_XAR;
+// Bitcoin
+$historical['VEF_BTC'] = $historical_XVE_BTC;
+$historical['ARS_BTC'] = $historical_XAR_BTC;
+
+// Litecoin
+$historical['VEF_LTC'] = $historical_XVE_LTC;
+$historical['ARS_LTC'] = $historical_XAR_LTC;
 
 echo json_encode($historical);
 

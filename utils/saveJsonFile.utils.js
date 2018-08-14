6626e4a8162484322f2339a8c1800453bf3d5b3e
= <=>
2018-08-14 17:30:49 -0400

    save json file

--- utils/saveJsonFile.utils.js
+++ utils/saveJsonFile.utils.js
@@ -21,21 +21,14 @@ require('mongoose-pagination')
 const savejsonfile = (historicCoin) => {
     var jsonData = formatHistoric(historicCoin)
     console.log(jsonData);
-    
     var file = '../tmp/historic/data.json'
-    /* fs.chmod(file, 777, function(err){
-        if(err){ */
-            jsonfile.writeFile('data.json', jsonData, function (err) {
-                if (err) {
-                 debug("An error occured while writing JSON Object to File.")
-                }else {
-                    debug("JSON file has been saved.")
-                }
-            });
-        /* }else {
-            debug("An error occured while permission JSON File.")
+    jsonfile.writeFile(file, jsonData, function (err) {
+        if (err) {
+            debug("An error occured while writing JSON Object to File.")
+        }else {
+            debug("JSON file has been saved.")
         }
-    }) */
+    })
 }
 
 module.exports = historicCoinQuery

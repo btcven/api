d0d6e59b8c3d0b92299a61c438b4f080b5cd71b9
= <=>
2018-08-14 21:03:14 -0400

    save json file

--- api/src/historic-coin/historic-coin.controller.js
+++ api/src/historic-coin/historic-coin.controller.js
@@ -3,13 +3,12 @@ const chalk = require('chalk')
 const debug = require('debug')('btcven-api-v2:historic-coin')
 require('mongoose-pagination')
 const  jsonfile = require('jsonfile')
- async function historicCoinQuery(req,res){
+  const  historicCoinQuery = (req,res) => {
 
 jsonfile.readFile('historic.json', function(err, obj) {
     if (err) {
         debug("An error occured while reading JSON Object to File.")
     }
-    console.dir(obj)
     res.status(200).send(obj)
 })
 }

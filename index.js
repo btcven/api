9cd95b7c152e57535487d106765f7cf932fc05bf
= <=>
2018-08-19 15:15:17 -0400

    modificacion de puerto

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ require('./config/mongo-db')()
 .then(()=> SaveDataDB())
 .catch((error) =>{throw new Error(error)})
 // Conexion con redisDB
-require('./config/redis-db').initRedisDB
-.then((redisClient)=> scheduleCoin(redisClient))
-.catch((error) =>{throw new Error(error)})
+// require('./config/redis-db').initRedisDB
+// .then((redisClient)=> scheduleCoin(redisClient))
+// .catch((error) =>{throw new Error(error)})
 // Inicio del server
 api.listen(port, () => console.log(`${chalk.green('[btven-api-v2]')} server listening on port ${port}`))

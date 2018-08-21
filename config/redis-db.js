ccf160e1a37cddaf4c20b171acee50ace079bf16
Diego Fernández Barrera <bigomby@gmail.com>
2018-08-21 09:23:00 +0200

    Allow redis and mongo to be configured via ENVs

--- config/redis-db.js
+++ config/redis-db.js
@@ -1,6 +1,10 @@
 var redis = require('redis')
 const chalk = require('chalk')
-const redisClient =  redis.createClient({host : 'localhost', port : 6379})
+
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
+const REDIS_PORT = process.env.REDIS_PORT || 6739
+
+const redisClient =  redis.createClient({host : REDIS_HOST, port : REDIS_PORT})
 const initRedisDB = new Promise((resolve, reject) =>{
     redisClient.on('ready',() => {
         resolve(redisClient)

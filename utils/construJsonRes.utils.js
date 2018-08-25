98eada75c1c6f7cbb96f426df6e7473a029ba69b
= <=>
2018-08-25 12:13:25 -0400

    integracion de versionado al proyecto

--- utils/construJsonRes.utils.js
+++ utils/construJsonRes.utils.js
@@ -6,6 +6,8 @@ module.exports = reply => {
                         "VEF": Number(reply.BTCVEF),
                         "USD": Number(reply.BTCUSD),
                         "EUR": Number(reply.BTCEUR),
+                        "XMR": Number(reply.BTCXMR),
+                        "ETH": Number(reply.BTCETH),
                         "LTC": Number(reply.BTCLTC),
                         "ARS": Number(reply.BTCARS)
                     },
@@ -13,8 +15,28 @@ module.exports = reply => {
                         "VEF": Number(reply.LTCVEF),
                         "USD": Number(reply.LTCUSD),
                         "EUR": Number(reply.LTCEUR),
+                        "XMR": Number(reply.LTCXMR),
+                        "ETH": Number(reply.LTCETH),
                         "ARS": Number(reply.LTCARS),
                         "BTC": Number(reply.LTCBTC)
+                    },
+            'XMR':  {   
+                        "VEF": Number(reply.XMRVEF),
+                        "USD": Number(reply.XMRUSD),
+                        "EUR": Number(reply.XMREUR),
+                        "ETH": Number(reply.XMRETH),
+                        "LTC": Number(reply.XMRLTC),
+                        "ARS": Number(reply.XMRARS),
+                        "BTC": Number(reply.XMRBTC)
+                    },
+            'ETH':  {   
+                        "VEF": Number(reply.ETHVEF),
+                        "USD": Number(reply.ETHUSD),
+                        "EUR": Number(reply.ETHEUR),
+                        "LTC": Number(reply.ETHLTC),
+                        "XMR": Number(reply.ETHXMR),
+                        "ARS": Number(reply.ETHARS),
+                        "BTC": Number(reply.ETHBTC)
                     },
   'exchange_rates': {   
                         "EUR_USD": Number(reply.USDEUR),
